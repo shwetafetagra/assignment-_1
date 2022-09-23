6c884af44e809f6ce6aecb812e864e36479683b0
shwetafetagra <87521557+shwetafetagra@users.noreply.github.com>
2022-09-23 17:41:07 +0530

    Add files via upload

---
+++ odd_even.py
@@ -0,0 +1,13 @@
+from itertools import count
+
+
+num = (1,2,3,4,5,6,7,8,9,10,11) 
+num_odd = 0
+num_even = 0
+for x in num:
+    if not x%2:
+        num_even+=1
+    else:
+        num_odd+=1   
+print("numbers of even no. :",num_even)
+print("numbers of odd no. : ", num_odd)         
